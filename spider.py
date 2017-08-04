ac207e069609c1c58ee94318b645cac25ef60c4c
Daniel Thurau <daniel.n.thurau@gmail.com>
2017-08-04 11:00:19 -0700

    pruned, but still cant fix directory hop

--- spider.py
+++ spider.py
@@ -5,6 +5,7 @@
 from lxml import html
 import requests
 import json
+import re
 
 
 # Adjacency list containing lists. 
@@ -23,7 +24,13 @@ def checkDomain(url, domain):
 def getLinks(url):
 	page = requests.get(url)
 	webpage = html.fromstring(page.content)
-	return webpage.xpath('//a/@href')
+	bases = webpage.xpath('//base/@href')
+	for base in bases:
+		print(base)
+	hrefs =  webpage.xpath('//a/@href')
+	links = webpage.xpath('//link/@href')
+	return (links + hrefs)
+
 
 # def getSearch(url):
 # 	page = requests.get(url)
@@ -33,60 +40,84 @@ def getLinks(url):
 # 	for item in items:
 # 		if returnable 
 
+def clean(links):
+	global global_start_url
+	newlinks = []
+	for link in links:
+		if "file://" in link:
+			newlinks.append(link)
+			continue
+		link = link.replace("../","")
+		link = link.replace("..","")
+		if link == global_start_url:
+			continue
+		if "//" in link:
+			for m in re.finditer('//', link):
+				if link[int(m.start()-1)] != ":":
+					link = link[:int(m.start())] + link[int(m.start()+1):]
+		newlinks.append(link)
+	return newlinks
 
-def addVertice(vertice):
-	global adj
+def addVertice(vertice, adj):
 	adj[vertice] = [];
 
 
-def addNeighbors(vertice, neighborList):
+def prune(url, delimeter):
+	while url[-1] != delimeter:
+		url = url[:-1]
+
+	return url;
+	
+
+def addNeighbors(vertice, adj, neighborList):
 	print(("Adding neighbors for vertice:" + vertice))
-	global adj
 	global global_allowed_domain
 	global global_start_url
+
+
 	for neighbor in neighborList:
 		# neighbor = neighbor.replace("../", "")
 		if(checkDomain(neighbor, global_allowed_domain)):
+
+			if "?" in neighbor and vertice[-1] != "/" and neighbor[0] != "/":
+				url = prune(vertice, "/")
+				neighbor = url + neighbor
 			if 'http' in neighbor:
 				adj[vertice].append(neighbor)
 			else:
 				adj[vertice].append(global_start_url + neighbor)
+	return adj[vertice]
 
-def checkHop(url):
-	# print("Entered check hop")
-	global adj
+def checkHop(url, adj):
 	global global_start_url
-	# print("-----------------------------------------------------")
-	# print(adj[url])
-	# url_ex = url.split("/");
+
+
 	for neighbor in adj[url]:
-		# print(neighbor)
-		# neighbor = neighbor.replace("../", "")
+		if "file://" in neighbor:
+			 continue
 		neighbor_ex = neighbor.split("/")
 		neighbor_ex = neighbor_ex[:-1]
 		neighbor_ex = "/".join(neighbor_ex) + "/"
 		neighbor_ex.strip()
-		# neighbor_ex = neighbor_ex.replace("//","/")
-		# print("		" + neighbor_ex)
-		# count = 0;
-		# print(global_start_url);
 		while(neighbor_ex  != global_start_url):
-			# print("		" + neighbor_ex)
 			if neighbor_ex not in adj[url]:
-				# print("Not in neighbors")
-				adj[url].append(neighbor_ex[:-1])
+				adj[url].append(neighbor_ex)
 
 			neighbor_ex = neighbor_ex.split("/")
-			# print(neighbor_ex)
 			neighbor_ex = neighbor_ex[:-1]
 			neighbor_ex = neighbor_ex[:-1]
-			# print(neighbor_ex)
 			neighbor_ex = "/".join(neighbor_ex) + "/"
-			# print("		" + neighbor_ex)
 			neighbor_ex.strip()
-			# neighbor_ex = neighbor_ex.replace("//","/")
-		# 	count+=1;
+	return adj[url]
 
+def checkQ(url, adj):
+	global global_start_url
+
+	if "?" in url:
+		newUrl = prune(url, "?")
+		if newUrl not in adj[url]:
+			adj[url].append(newUrl)
+	return adj[url]
 
 def BFS(s, adj):
 	# Global variable adjaceny list. Not built because of unknown data
@@ -100,9 +131,10 @@ def BFS(s, adj):
 		nextItem = []
 		for u in frontier:
 			if u not in adj:
-				addVertice(u);
-				addNeighbors(u,getLinks(u));
-				checkHop(u)
+				adj[u] = [];
+				adj[u] = addNeighbors(u, adj, getLinks(u));
+				adj[u] = checkHop(u, adj)
+				adj[u] = clean(adj[u])
 			for v in adj[u]:
 				if v not in level:
 					level[v] = i
@@ -115,39 +147,20 @@ def BFS(s, adj):
 
 
 
-def preOrderPrint(url, adj, i, visited):
-	for i in range(0,i):
-		print("   ", end="")
-	print(url)
-	items = adj[url]
-	for u in items:
-		if u != url and u not in visited:
-			visited[u] = 1
-			preOrderPrint(u, adj, i+1, visited)
-		# else:
-		# 	for i in range(0,i):
-		# 		print("   ", end="")
-		# 	print(u) 
-	# return
-
-def pretty(d, indent=0):
-   for key, value in d.iteritems():
-      print('\t' * indent + str(key))
-      if isinstance(value, dict):
-         pretty(value, indent+1)
-      else:
-         print('\t' * (indent+1) + str(value))
 
 
-
-
-
-
-# global_start_url = input("Enter starting URL: ")
-# global_allowed_domain = input("Enter allowed domains: ")
 global_start_url = "http://demo.testfire.net/"
 global_allowed_domain = "demo.testfire.net"
 
+
+
+# test = "http://demo.testfire.net/bank/ass"
+# print(test)
+# print(getVertice(test))
+
+# test2 = "http://demo.testfire.net/bank/"
+# print(test2)
+# print(getVertice(test2))
 
 
 
@@ -155,17 +168,12 @@ global_allowed_domain = "demo.testfire.net"
 hold = BFS(global_start_url, adj);
 
 
-# print(json.dumps(adj, indent=1))
-# pretty(adj);
-print("--------------------------------------------")
-visited = {}
-preOrderPrint(global_start_url, adj, 0, visited);
 
-# preOrderPrint 
-# print(hold);
-# print(len(hold));
+# # preOrderPrint 
+# # print(hold);
+print(len(hold));
 
 
-# for x in hold:
-# 	print(str(x))
+for x in hold:
+	print(str(x))
 
